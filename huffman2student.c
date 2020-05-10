fad2da45ce88a2ad7fe585437afccca56d499efc
Axe <52039717+Maxorao@users.noreply.github.com>
2020-05-10 17:53:36 +0800

    Update huffman2student.c
    This is a project of BUAA for student in grade one to complish. The main body of the program is done by the teacher and the rest part will be finish by the student as homework.

--- huffman2student.c
+++ huffman2student.c
@@ -1,58 +1,57 @@
-//�ļ�ѹ��-Huffmanʵ��
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+//文件压缩-Huffman实现
+#include  <stdio.h>
+#include  <string.h>
+#include  <stdlib.h>
 
-#define MAXSIZE 32
+#define  MAXSIZE  32
 
-struct tnode {					//Huffman���ṹ
-	char c;		
-	int weight;					//���ڵ�Ȩ�أ�Ҷ�ڵ�Ϊ�ַ������ĳ��ִ���
-	struct tnode *left,*right;
-} ; 
-int Ccount[128]={0};			//���ÿ���ַ��ĳ��ִ�������Ccount[i]��ʾASCIIֵΪi���ַ����ִ��� 
-struct tnode *Root=NULL; 		//Huffman���ĸ��ڵ�
-char HCode[128][MAXSIZE]={{0}}; //�ַ���Huffman���룬��HCode['a']Ϊ�ַ�a��Huffman���루�ַ�����ʽ�� 
-int Step=0;						//ʵ�鲽�� 
-FILE *Src, *Obj;
-	
-void statCount();				//����1��ͳ���ļ����ַ�Ƶ��
-void createHTree();				//����2������һ��Huffman�������ڵ�ΪRoot 
-void makeHCode();				//����3������Huffman������Huffman����
-void atoHZIP(); 				//����4������Huffman���뽫ָ��ASCII���ı��ļ�ת����Huffman���ļ�
+struct  tnode  {                        //Huffman树结构
+        char  c;                
+        int  weight;                //树节点权重，叶节点为字符和它的出现次数
+        struct  tnode  *left,*right;
+}  ;  
+int  Ccount[128]={0};                //存放每个字符的出现次数，如Ccount[i]表示ASCII值为i的字符出现次数  
+struct  tnode  *Root=NULL;          //Huffman树的根节点
+char  HCode[128][MAXSIZE]={{0}};  //字符的Huffman编码，如HCode['a']为字符a的Huffman编码（字符串形式）  
+int  Step=0;                        //实验步骤  
+FILE  *Src,  *Obj;
+        
+void  statCount();                //步骤1：统计文件中字符频率
+void  createHTree();                //步骤2：创建一个Huffman树，根节点为Root  
+void  makeHCode();                //步骤3：根据Huffman树生成Huffman编码
+void  atoHZIP();                  //步骤4：根据Huffman编码将指定ASCII码文本文件转换成Huffman码文件
 
-void print1();					//�������1�Ľ��
-void print2(struct tnode *p);	//�������2�Ľ�� 
-void print3();					//�������3�Ľ��
-void print4();					//�������4�Ľ��
+void  print1();                        //输出步骤1的结果
+void  print2(struct  tnode  *p);        //输出步骤2的结果  
+void  print3();                        //输出步骤3的结果
+void  print4();                        //输出步骤4的结果
 
-int main()
+int  main()
 {
-	if((Src=fopen("input.txt","r"))==NULL) {
-		fprintf(stderr, "%s open failed!\n", "input.txt");
-		return 1;
-	}
-	if((Obj=fopen("output.txt","w"))==NULL) {
-		fprintf(stderr, "%s open failed!\n", "output.txt");
-		return 1;
-	}
-	scanf("%d",&Step);					//���뵱ǰʵ�鲽�� 
-	
-	statCount();						//ʵ�鲽��1��ͳ���ļ����ַ����ִ�����Ƶ�ʣ�
-	(Step==1) ? print1(): 1; 			//���ʵ�鲽��1���	
-	createHTree();						//ʵ�鲽��2�������ַ�Ƶ��������Ӧ��Huffman��
-	(Step==2) ? print2(Root): 2; 		//���ʵ�鲽��2���	
-	makeHCode(Root);					//ʵ�鲽��3������RootΪ���ĸ���Huffman��������ӦHuffman����
-	(Step==3) ? print3(): 3; 			//���ʵ�鲽��3���
-	(Step>=4) ? atoHZIP(),print4(): 4; 	//ʵ�鲽��4����Huffman��������ѹ���ļ��������ʵ�鲽��4���	
+                if((Src=fopen("input.txt","r"))==NULL)  {
+                        fprintf(stderr,  "%s  open  failed!\n",  "input.txt");
+                return  1;  
+        }
+        if((Obj=fopen("output.txt","w"))==NULL)  {
+                fprintf(stderr,  "%s  open  failed!\n",  "output.txt");
+                return  1;
+        }
+        scanf("%d",&Step);                //输入当前实验步骤  
+        
+        statCount();                        //实验步骤1：统计文件中字符出现次数（频率）
+        (Step==1)?print1():1;              //输出实验步骤1结果        
+        createHTree();                        //实验步骤2：依据字符频率生成相应的Huffman树
+        (Step==2)?print2(Root):2;          //输出实验步骤2结果        
+        makeHCode();                //实验步骤3：依据Root为树的根的Huffman树生成相应Huffman编码
+        (Step==3)?print3():3;              //输出实验步骤3结果
+        (Step>=4)?atoHZIP(),print4():4;//实验步骤4：据Huffman编码生成压缩文件，并输出实验步骤4结果        
 
-	fclose(Src);
-	fclose(Obj);
+        fclose(Src);
+        fclose(Obj);
+                return  0;
+}  
 
-    return 0;
-} 
-
-//��ʵ�鲽��1����ʼ 
+//【实验步骤1】开始  
 char Text[10000]={'\0'};
 void statCount()
 {
@@ -68,10 +67,12 @@ void statCount()
 	Ccount[0]=1;
 }
 
-//��ʵ�鲽��1������
 
-//��ʵ�鲽��2����ʼ
 
+
+//【实验步骤1】结束
+
+//【实验步骤2】开始
 typedef struct Node
 {
 	struct tnode* self;
@@ -157,10 +158,12 @@ void createHTree()
 }
 
 
-//��ʵ�鲽��2������
 
-//��ʵ�鲽��3����ʼ
 
+
+//【实验步骤2】结束
+
+//【实验步骤3】开始
 char Code[9]={'\0'};
 void GetCode(char c,char Code[])
 {
@@ -202,12 +205,12 @@ void makeHCode()
 
 
 
-//��ʵ�鲽��3������
-
-//��ʵ�鲽��4����ʼ
 
 
 
+//【实验步骤3】结束
+
+//【实验步骤4】开始
 void atoHZIP()
 {
 	int i=0,j=0,k=0;
@@ -268,61 +271,64 @@ void atoHZIP()
 	
 }
 
-//��ʵ�鲽��4������
 
-void print1()
+
+
+//【实验步骤4】结束
+
+void  print1()
 {
-	int i;
-	printf("NUL:1\n");
-	for(i=1; i<128; i++)
-		if(Ccount[i] > 0)
-			printf("%c:%d\n", i, Ccount[i]);
+        int  i;
+        printf("NUL:1\n");
+        for(i=1;  i<128;  i++)
+                if(Ccount[i]  >  0)
+                        printf("%c:%d\n",  i,  Ccount[i]);
 }
 
-void print2(struct tnode *p)
+void  print2(struct  tnode  *p)
 {
-	if(p != NULL){
-		if((p->left==NULL)&&(p->right==NULL)) 
-			switch(p->c){
-				case 0: printf("NUL ");break;
-				case ' ':  printf("SP ");break;
-				case '\t': printf("TAB ");break;
-				case '\n':  printf("CR ");break;
-				default: printf("%c ",p->c); break;
-			}
-		print2(p->left);
-		print2(p->right);
-	}
+        if(p  !=  NULL){
+                if((p->left==NULL)&&(p->right==NULL))  
+                        switch(p->c){
+                                case  0:  printf("NUL  ");break;
+                                case  '  ':    printf("SP  ");break;
+                                case  '\t':  printf("TAB  ");break;
+                                case  '\n':    printf("CR  ");break;
+                                default:  printf("%c  ",p->c);  break;
+                        }
+                print2(p->left);
+                print2(p->right);
+        }
 }
 
-void print3()
+void  print3()
 {
-	int i;
-	
-	for(i=0; i<128; i++){
-		if(HCode[i][0] != 0){
-			switch(i){
-				case 0: printf("NUL:");break;
-				case ' ':  printf("SP:");break;
-				case '\t': printf("TAB:");break;
-				case '\n':  printf("CR:");break;
-				default: printf("%c:",i); break;
-			}
-			printf("%s\n",HCode[i]);
-		}
-	}
-} 
+        int  i;
+        
+        for(i=0;  i<128;  i++){
+                if(HCode[i][0]  !=  0){
+                        switch(i){
+                                case  0:  printf("NUL:");break;
+                                case  '  ':    printf("SP:");break;
+                                case  '\t':  printf("TAB:");break;
+                                case  '\n':    printf("CR:");break;
+                                default:  printf("%c:",i);  break;
+                        }
+                        printf("%s\n",HCode[i]);
+                }
+        }
+}  
 
-void print4()
+void  print4()
 {
-	long int in_size, out_size;
-	
-	fseek(Src,0,SEEK_END);
-	fseek(Obj,0,SEEK_END);
-	in_size = ftell(Src);
-	out_size = ftell(Obj);
-	
-	printf("\n原文件大小：%ldB\n",in_size);
-	printf("压缩后文件大小：%ldB\n",out_size);
-	printf("压缩率：%.2f%%\n",(float)(in_size-out_size)*100/in_size);
+        long  int  in_size,  out_size;
+        
+        fseek(Src,0,SEEK_END);
+        fseek(Obj,0,SEEK_END);
+        in_size  =  ftell(Src);
+        out_size  =  ftell(Obj);
+        
+        printf("\n原文件大小：%ldB\n",in_size);
+        printf("压缩后文件大小：%ldB\n",out_size);
+        printf("压缩率：%.2f%%\n",(float)(in_size-out_size)*100/in_size);
 }
